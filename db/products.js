000e66002c321257894bd4db6bb2a84369f8e728
duyduc130602 <nguyennam77223@gmail.com>
2022-04-29 20:34:49 +0700

    upload

--- db/products.js
+++ db/products.js
@@ -2,7 +2,7 @@
     {
      "name": "Cao Thị Hoa Trang ❤ Liêm",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh2.jpg",
+     "anh": "anh2.JPG",
      "url1": "nlll",
      "url2": "https://www.facebook.com/profile.php?id=100048363498523"
     },
@@ -16,14 +16,14 @@
     {
      "name": "Nguyễn Thị h.Yến  ❤ Huy Nam",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh5.jpg",
+     "anh": "anh5.JPG",
      "url1": "https://www.facebook.com/huynamhth12",
      "url2": "https://www.facebook.com/profile.php?id=100062390836088"
     },
     {
      "name": "Thao Nguyen  ❤ Giấu mắt",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh6.jpg",
+     "anh": "anh6.JPG",
      "url1": "",
      "url2": "https://www.facebook.com/profile.php?id=100010058260780"
     },
@@ -37,7 +37,7 @@
     {
      "name": "Ngọc Anh   ❤ My Chu",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh8.jpg",
+     "anh": "anh8.JPG",
      "url1": "https://www.facebook.com/my.kieu.35325074",
      "url2": ""
     },
@@ -51,105 +51,105 @@
     {
      "name": "Hồ Mạnh Đại   ❤ Hải Him",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh9.jpg",
+     "anh": "anh9.JPG",
      "url1": "https://www.facebook.com/profile.php?id=100011152476194",
      "url2": ""
     },
     {
      "name": "Đăng Quang Khánh   ❤ Giấu mặt",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh10.jpg",
+     "anh": "anh10.JPG",
      "url1": "https://www.facebook.com/1212142545343Qk",
      "url2": ""
     },
     {
      "name": "Nhi Anh   ❤ Võ đạt",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh11.jpg",
+     "anh": "anh11.JPG",
      "url1": "https://www.facebook.com/profile.php?id=100031310527848",
      "url2": "https://www.facebook.com/nhi.anhh.146"
     },
     {
      "name": "Nguyễn Khắc Huy   ❤ Thùy Dương",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh12.jpg",
+     "anh": "anh12.JPG",
      "url1": "https://www.facebook.com/EmTeo99?comment_id=Y29tbWVudDo2ODk4MjUzOTUzOTk3NDZfNjg5ODI3MTEyMDY2MjQx",
      "url2": "https://www.facebook.com/thuy.duonnggg"
     },
     {
      "name": "Minh Vương ❤ Nhi",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh3.jpg",
+     "anh": "anh3.JPG",
      "url1": "https://www.facebook.com/profile.php?id=100026521878792",
      "url2": "https://www.facebook.com/profile.php?id=100041726520678"
     },
     {
      "name": "Đào Thúy An ❤ Nguyên Quốc Tuấn",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh13.jpg",
+     "anh": "anh13.JPG",
      "url1": "https://www.facebook.com/profile.php?id=100026521878792",
      "url2": "https://www.facebook.com/profile.php?id=100004875018647"
     },
     {
      "name": "Phan Thị Xuân ❤ Đạt Lee",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh14.jpg",
+     "anh": "anh14.JPG",
      "url1": "https://www.facebook.com/profile.php?id=100052105388901",
      "url2": "https://www.facebook.com/giangdat99"
     },
     {
      "name": "Phạm Khắc Chiến ❤ Tạ Thị Thu Ánh",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh15.jpg",
+     "anh": "anh15.JPG",
      "url1": "https://www.facebook.com/profile.php?id=100054284795880",
      "url2": "https://www.facebook.com/anhne.choioi"
     },
     {
      "name": "Linh Xuân ❤ Minh dũng",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh16.jpg",
+     "anh": "anh16.JPG",
      "url1": "https://www.facebook.com/dung.bin.562",
      "url2": "https://www.facebook.com/minhdung.nguyen.9822"
     },
     {
      "name": "Phương Thảo  ❤ Xuân Phước",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh17.jpg",
+     "anh": "anh17.JPG",
      "url1": "https://www.facebook.com/phuoc.nguyenxuan.940436",
      "url2": "https://www.facebook.com/bes.pis.92"
     },
     {
      "name": "Hà Lê ❤ Nguyễn đức Anh",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh18.jpg",
+     "anh": "anh18.JPG",
      "url1": "https://www.facebook.com/profile.php?id=100068249053041",
      "url2": "https://www.facebook.com/profile.php?id=100048151961948"
     },
     {
      "name": "Thương Hạnh ❤ Nguyễn Dung ",
      "note": "Chúc Hạnh Phúc",
-     "anh": "anh19.jpg",
+     "anh": "anh19.JPG",
      "url1": "https://www.facebook.com/thuong.hanh.24122002",
      "url2": "https://www.facebook.com/lunn.namm.3990?comment_id=Y29tbWVudDo2Njc2MDU3OTQzNzU0MDRfNjY3NjM1MzU3NzA1Nzgx"
     },
     {
      "name": "Trần Minh Nam ❤ Tạ Thị Quỳnh Trúc ",
      "note": "Đẩy Thuyền Nha",
-     "anh": "anh20.jpg",
+     "anh": "anh20.JPG",
      "url1": "https://www.facebook.com/nam.tranminh.33821189",
      "url2": "https://www.facebook.com/profile.php?id=100012201662276"
     },
     {
      "name": "Lê Ngọc Diệp ❤ Nguyễn thành Vinh ",
      "note": "Đẩy Thuyền Nha",
-     "anh": "anh21.jpg",
+     "anh": "anh21.JPG",
      "url1": "https://www.facebook.com/profile.php?id=100011592156121",
      "url2": "https://www.facebook.com/ngoc.diep.04122002"
     },
     {
      "name": "Nguyễn trí Nam ❤ Mạc Thị thảo ",
      "note": "Đẩy Thuyền Nha",
-     "anh": "anh22.jpg",
+     "anh": "anh22.JPG",
      "url1": "https://www.facebook.com/nam.nguyentri.161",
      "url2": "https://www.facebook.com/macthi.thao.5661"
     }
